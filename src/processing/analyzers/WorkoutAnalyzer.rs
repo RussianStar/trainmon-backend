2479b3935f92db0f133938aaa8e2502e6d969ca5
Tilman Ruß <tilman.russ@aurum-engineering.de>
2023-11-11 19:50:52 +0100

    WIP : Implement time of workout

--- src/processing/analyzers/WorkoutAnalyzer.rs
+++ src/processing/analyzers/WorkoutAnalyzer.rs
@@ -1,11 +1,19 @@
+use fitparser;
+
 use crate::model::enums::PartialResult;
 use crate::model::enums::WorkoutSummary;
 use crate::model::traits::Analyzer;
+
 
 pub struct WorkoutAnalyzer;
 impl Analyzer for WorkoutAnalyzer {
     fn analyze(&self, timeslice: &fitparser::FitDataRecord) -> Option<PartialResult> {
         // Logic for WorkoutAnalyzer
-        Some(PartialResult::WorkoutData(WorkoutSummary { date: 0}))
+        if let MesgNum::Session = timeslice.kind() {
+            let start_time = timeslice.get_field("timestamp");
+            Some(PartialResult::WorkoutData(WorkoutSummary { date: start_time}))
+        }
+
+
     }
 }
